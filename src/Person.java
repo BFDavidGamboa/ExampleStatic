736ccb5fd8cc91e4b4912c91891d81c699a61a33
BFDavidGamboa <david.gamboa@blankfactor.com>
2023-08-16 15:03:27 -0500

    Created static parameters to prove static concept inside a variable usage in method call

--- src/Person.java
+++ src/Person.java
@@ -4,7 +4,31 @@ public class Person {
     private static int personCount;// Associated with the class
 
     public Person(String name) {
-        this.personId =
+        this.personId = ++personCount;
+        this.name = name;
     }
 
+    public int getPersonId() {
+        return this.personId;
+    }
+    public void setPersonId(int personId){
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static int getPersonCount(){
+        return personCount;
+    }
+
+    @Override
+    public String toString(){
+        return "personId: " + personId + ", name: " + name + ", personCount: " + personCount;
+    }
 }
